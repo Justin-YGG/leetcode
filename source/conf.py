e22d67c10ac827b4dacb1b3f72c5812b958af3d8
Justin <lijiaqing@licaigc.com>
2020-01-04 00:06:51 +0800

    refine

--- source/conf.py
+++ source/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = u'en'
+language = None
 # html_search_options = {'dict': 'user_dict'}
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
